2d7040ca2bc6dcea132d664e40a0df411e66a7c3
YuJiaLiang0702 <ch993115@gmail.com>
2024-07-10 23:02:23 +0800

    change

---
+++ VolleyBall Scoreboard/VolleyBall Scoreboard/VolleyBall_ScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyBall_ScoreboardApp.swift
+//  VolleyBall Scoreboard
+//
+//  Created by Jesse Liang on 2024/6/25.
+//
+
+import SwiftUI
+
+@main
+struct VolleyBall_ScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
